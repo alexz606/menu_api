3b5b76e00a21b1671bc87aebafb0c669af4563d5
Rene Zablah <zablahre@yahoo.com>
2020-10-05 20:10:07 -0400

    updated form to use a pick list

--- menu/src/main/java/tech/base2/menu/menuController.java
+++ menu/src/main/java/tech/base2/menu/menuController.java
@@ -6,11 +6,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,19 +23,51 @@ public class menuController {
 	@Autowired
 	private MenuService menuService;
 	
-	//Return All Items
-	@GetMapping("/menu")
-	public String greeting(Model model) {
-		model.addAttribute("menu", menuService.getWeeklyMenu());
-		return "greeting";
+	@ModelAttribute("daysOfTheWeek")
+	public List<String> populateDays() {
+	    return Arrays.asList("Select Day", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+	}
+
+	@ModelAttribute("proteinChoices")
+	public List<String> populateProteins() {
+	    return Arrays.asList("Select Protein", "fish", "chicken", "salmon", "ground beef", "steak", "pork chops");
+	}
+
+	
+	@ModelAttribute("carbChoices")
+	public List<String> populateCarbs() {
+	    return Arrays.asList("Select Carb", "rice", "pasta", "quinoa", "french fries", "mashed potato");
 	}
 	
-	//Return an item
-	@GetMapping("/menu/{day}")
-	public String getASingleItem(@PathVariable String day, Model model) {
-		model.addAttribute("menu", menuService.getDailyMenu(day));
-		return "greeting";
+	//Returns a form
+	@GetMapping("/menu")
+	public String menuForm(Model model) {
+		model.addAttribute("menu", new Menu());
+		return "menu";
 	}
+
+	//Returns all menu items including the newly created item
+	@PostMapping("/menu")
+	public String menuSubmit(@ModelAttribute Menu menu, Model model) {
+		//model.addAttribute("menu", menu);
+		menuService.addMenu(menu);
+		model.addAttribute("menu", menuService.getWeeklyMenu());
+		return "result";
+	}
+
+    //Return All Items
+  //  @GetMapping("/")
+  //  public String greeting(Model model) {
+    //        model.addAttribute("menu", menuService.getWeeklyMenu());
+      //      return "greeting";
+    //}
+	
+	//Return an item
+//	@GetMapping("/menu/{day}")
+//	public String getASingleItem(@PathVariable String day, Model model) {
+//		model.addAttribute("menu", menuService.getDailyMenu(day));
+//		return "greeting";
+//	}
 
 //	@RequestMapping("/{day}")
 //	public Menu getASingleItem(@PathVariable String day) {
